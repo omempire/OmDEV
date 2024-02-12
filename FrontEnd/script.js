151cf4de65b9aa82e89a117a257c10562608454e
omempire <omcloudlive@gmail.com>
2024-02-12 21:22:47 +0530

    frontend updated

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,11 +3,11 @@ let hamBtn = document.getElementById("nav-toggler");
 
 hamBtn.addEventListener("click", function() {
 
-    document.getElementById('logo').style.display = "none";
+    document.getElementById('logoName').style.display = "none";
     
     let menuDiv = document.getElementById('navbar');
     menuDiv.style.display = "block";
-    menuDiv.style.gap = "1px";
+    
 
     let menuList = menuDiv.getElementsByTagName('a');
     let size = menuList.length;
