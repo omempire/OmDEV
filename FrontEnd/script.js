ba6a4500178a38b37a998bf4114b59e3f9aa82fb
omempire <omcloudlive@gmail.com>
2024-02-13 21:21:19 +0530

    frontend updated

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,27 +1,7 @@
 
 // HAMBURGER SCRIPT
 
-let hamBtn = document.getElementById("nav-toggler");
-let winWidth = window.innerWidth;
-let winHeight = window.innerHeight;
+// let hamBtn = document.getElementById("nav-toggler");
+// let winWidth = window.innerWidth;
+// let winHeight = window.innerHeight;
 
-hamBtn.addEventListener('click', function() {
-
-    let logoNm = document.getElementById("logoName");
-    let menuDiv = document.getElementById("navbarLinks2");
-
-    if (logoNm.classList.contains("active")) {
-
-        logoNm.classList.add("hidden");
-        logoNm.classList.remove("active");
-
-        menuDiv.classList.add("active");
-        menuDiv.classList.remove("hidden");
-    }
-    else {
-
-        logoNm.classList.add("active");
-        menuDiv.classList.add("hidden");
-    }
-
-});
